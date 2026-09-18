81ff7e564d24d692df3d202f457d7e2f4ae3f59e
Muhammad ikbal <mmu49026@gmail.com>
2026-09-18 21:31:49 +0700

    Refactor Android build configuration in build.gradle.kts
    Updated compileSdk version and reorganized defaultConfig settings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,20 +4,17 @@ plugins {
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
 }
-
 android {
-  namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
-
-  defaultConfig {
-    applicationId = "com.example.folia"
-    minSdk = 24
-    targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-  }
+    namespace = "com.example"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.example.folia"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
 
   signingConfigs {
     create("release") {
