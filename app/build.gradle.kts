e99619d250be7ff52639ac63a286f4cac11e1e67
Muhammad ikbal <mmu49026@gmail.com>
2026-09-18 22:02:33 +0700

    Upgrade compileSdk and targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 android {
     namespace = "com.example"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.example.folia"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
